4dab89ccdc8eb3ab437c520c7ce92f02228cc943
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2023-06-24 14:00:00 -0500

    [BAS-39] Added Post Bill endpoint

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express, { Router } from "express";
+import { createBill } from "./bill/billController";
 
-const router = express.Router();
+const router: Router = express.Router();
 
 router.get("/heartbeat", (req: any, res: any) => {
     res.send('Pong');
 });
 
+router.post("/bill", createBill);
 
 export default router;
