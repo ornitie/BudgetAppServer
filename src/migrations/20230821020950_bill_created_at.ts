79e385a0a82b771eb7f8d60357ed18a14e90291d
Juan Pablo Rodriguez Morales <uncorreodeinternet@gmail.com>
2023-07-19 14:00:00 -0500

    [BAS-54] Added timestamp to bill

---
+++ src/migrations/20230821020950_bill_created_at.ts
@@ -0,0 +1,14 @@
+import { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('bill', (table) => {
+    table
+      .timestamp('created_at').defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.alterTable('bill', (table) => {
+    table.dropColumn('created_at');
+  });
+}
